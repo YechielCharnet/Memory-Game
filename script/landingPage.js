dc3374d86357d265d29ec588590816e056fed371
YechielCharnet <yechiel1995@gmail.com>
2024-06-05 19:45:44 +0300

    local storege update

--- script/landingPage.js
+++ script/landingPage.js
@@ -1,2 +1,33 @@
 "use strict";
 
+let name = null;
+let email = null;
+let password = null;
+let usersArray = [];
+
+function saveUser() {
+  name = document.getElementById("username").value;
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+  let tempUsersArray = localStorage.getItem("usersArray");
+
+  if (name && email && password) {
+    if (tempUsersArray) {
+      usersArray = JSON.parse(tempUsersArray);
+      for (let i = 0; i < usersArray.length; i++) {
+        if (email === usersArray[i]["email"]) {
+          alert("AAAAAAA");
+          break;
+        }
+      }
+    }
+    usersArray.push({
+      name: name,
+      email: email,
+      password: password,
+    });
+    localStorage.setItem("usersArray", JSON.stringify(usersArray));
+  }
+}
+console.log(usersArray[0]);
+document.querySelector("button").addEventListener("click", saveUser);
